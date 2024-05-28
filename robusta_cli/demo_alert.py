02f8bf760b1b9d1ac93683ae2d6ad6067894b1de
Tomer Keshet <tomer@robusta.dev>
2024-05-28 09:40:57 +0300

    made demo_alert work

--- robusta_cli/demo_alert.py
+++ robusta_cli/demo_alert.py
@@ -1,10 +1,66 @@
 import json
 import random
-from typing import List
+from typing import List, Optional
 
-from hikaru.model.rel_1_26 import Container, Job, JobSpec, ObjectMeta, PodSpec, PodTemplateSpec, SecurityContext
+from hikaru.model.rel_1_28 import Container, Job, JobSpec, ObjectMeta, PodSpec, PodTemplateSpec, SecurityContext
 from kubernetes import client, config
-from robusta.integrations.prometheus.utils import AlertManagerDiscovery
+
+from kubernetes.client import V1ServiceList
+from kubernetes.client.models.v1_service import V1Service
+
+import logging
+
+
+class AlertManagerDiscovery:
+
+    @staticmethod
+    def find_service_url(label_selector):
+        """
+        Get the url of an in-cluster service with a specific label
+        """
+        # we do it this way because there is a weird issue with hikaru's ServiceList.listServiceForAllNamespaces()
+        v1 = client.CoreV1Api()
+        svc_list: V1ServiceList = v1.list_service_for_all_namespaces(label_selector=label_selector)
+        if not svc_list.items:
+            return None
+        svc: V1Service = svc_list.items[0]
+        name = svc.metadata.name
+        namespace = svc.metadata.namespace
+        port = svc.spec.ports[0].port
+        cluster_domain = 'cluster.local'
+        url = f"http://{name}.{namespace}.svc.{cluster_domain}:{port}"
+        logging.info(f"discovered service with label-selector: `{label_selector}` at url: `{url}`")
+        return url
+
+    @staticmethod
+    def find_url(selectors: List[str], error_msg: str) -> Optional[str]:
+        """
+        Try to autodiscover the url of an in-cluster service
+        """
+        for label_selector in selectors:
+            service_url = AlertManagerDiscovery.find_service_url(label_selector)
+            if service_url:
+                return service_url
+
+        logging.debug(error_msg)
+        return None
+
+    @staticmethod
+    def find_alert_manager_url() -> Optional[str]:
+        return AlertManagerDiscovery.find_url(
+            selectors=[
+                "app=kube-prometheus-stack-alertmanager",
+                "app=prometheus,component=alertmanager",
+                "app=prometheus-operator-alertmanager",
+                "app=alertmanager",
+                "app=rancher-monitoring-alertmanager",
+                "app=prometheus-alertmanager",
+                "operated-alertmanager=true",
+                "app.kubernetes.io/name=alertmanager",
+                "app.kubernetes.io/name=vmalertmanager",
+            ],
+            error_msg="Alert manager url could not be found. Add 'alertmanager_url' under global_config",
+        )
 
 
 class AlertManagerException(Exception):
